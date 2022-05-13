f4784895d754f8266fac3efe5ab7a6718f3bd094
Darshan-V <mr.darshanv@gmail.com>
2022-05-13 11:07:31 +0530

    string parser working

--- jsonparser.js
+++ jsonparser.js
@@ -31,57 +31,35 @@ const spaceParser = function(input) {
     var Index = input.search(/^(\s)+/)
     while (Index === 0) {
       input = input.slice(Index + 1, input.length)
-      Index = input.search(/^(\s|\n|\t)+/)
+      Index = input.search(/^(\s|\n|\t|\b)+/)
     }
     if (Index === -1) { return [input] }
   }
 //string parser
 const stringParser = function(input){
-    if(input.startsWith('"')){
-        let i = 1
-        
-        let s = ''
-        while(input[i] !== '"'){
-            if(input[i] === '\\'){
-                s = s + input.substring(i, 2)
-                i += 2
-            }
-            else {
-                s = s + input[i]
-                i++
-            }
-        }
-        return [s, input.slice(i + 1)]
+  let i = 0
+  let spaceFound
+  let data
+  data = (spaceFound = spaceParser(input)) ? spaceFound[0] : input
+
+  if (data[0] === '"') {
+    data = data.substring(1, data.length)
+    i = data.search('"')
+    return ([data.substring(0, i), data.slice(i + 1, data.length)])
+  }
+  return null
+}
+
+//number parser
+    const numberParser = function(input){
+    const regex = /^\s*(-?(0|[1-9][0-9]*)(\.[0-9]+)?([eE][+-]?[0-9]+)?)\s*/
+    const matched = regex.exec(input)
+    if(matched !== null && matched.index === 0) {
+    return [ parseFloat(matched[1], input.trim().slice(matched[0].length))]
     }
     return null
 }
 
-//number parser
-// const numberParser = function(input){
-//     const regex = /^\s*(-?(0|[1-9][0-9]*)(\.[0-9]+)?(0|[eE][+-]?[0-9]+)?)\s*/
-//     // const regex = /^-?((0?0\.\d*)?([1-9]\d*(\.\d+)?))([eE][+-]?\d+)?/
-//     const matched = String(input).match(regex)
-//     if(!matched) return null
-//     return [ parseFloat(matched[0], input.slice(matched[0].length))]
-// }
-function numberParser (input) {
-    if (input[0] === '-' && /[^.e\d]/i.test(input[2]) && input[1] === '0') {
-      return [0, input.slice(2)]
-    }
-    if (input[0] === '0' && /[^.e\d]/i.test(input[1])) {
-      return [0, input.slice(1)]
-    }
-    const result = input.match(
-      /(^-?[1-9]\d*(\.\d+)?(e-?\d+)?)|(^-?\d\.\d+(e-?\d+)?)|(^-?0e-?\d+)/gi
-    )
-    if (result == null) return null
-    const resLength = result[0].length
-    let fResult = Number(input.slice(0, resLength))
-    if (fResult === 0) {
-      fResult = 0
-    }
-    return [fResult, input.slice(resLength)]
-}
 
 //array parser
 const arrayParser = function(input){
@@ -90,27 +68,28 @@ const arrayParser = function(input){
     if(!input.startsWith('[')) return null
     
     input = input.slice(1)
-    while(input){
+    while(true){
         result = spaceParser(input)
         if(result !== null) input = result[1]
         result = valueParser(input)
         if(result === null) break
         arr.push(result[0])
         input = result[1]
-        result = spaceParser(input)
-        if(result !== nullParser) return result[1]
         result = commaParser(input)
+        if(result !== nullParser) return result[1]
+        result = spaceParser(input)
         if(result === null) break
         input = result[i]
         if(input.startsWith(']')) return null
     }
     result = spaceParser(input)
     if(result !== null) 
-    input = result[1]
+    input = result[0]
     if(!input.startsWith(']')){
         return [arr, arr.slice(1)]
     }
     return null
+   
 }
 
 //value parser
@@ -118,6 +97,10 @@ const valueParser = function(input){
     let result
     result = /*objectParser(input) ||*/ arrayParser(input) || numberParser(input) || stringParser(input) || nullParser(input) || booleanParser(input)
     return result
+}
+
+const objectParser = function(input){
+
 }
 
 
@@ -129,11 +112,11 @@ const valueParser = function(input){
 // console.log(booleanParser('true'))
 // console.log(booleanParser('false123'))
 // console.log(stringParser('"hello123"'))
-// console.log(stringParser('"hello123\""'))
-// console.log(valueParser('+1.7e23abc'))
-console.log(numberParser('0.12'))
+console.log(stringParser('"hello123\u1235"'))
+// console.log(valueParser('-1.7e23'))
+// console.log(numberParser('e+1'))
 // console.log(valueParser('"hello"123'))
 // console.log(arrayParser('["Ford", "BMW", "Fiat"]'))
 // console.log(spaceParser('123'))
 
-// console.log(JSON.parse('0e+'))
+console.log(JSON.parse('"hello124\u1235"'))
